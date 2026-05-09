2248735933a85774fa3462f37211cea4ce71b539
satya2909 <mssm29092004@gmail.com>
2026-05-09 20:58:21 +0530

    first commit

---
+++ src/utils/api-responses.js
@@ -0,0 +1,10 @@
+class ApiResponses {
+    constructor(statusCode, data, message = "Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode < 400;
+    }
+}
+
+export {ApiResponses};
